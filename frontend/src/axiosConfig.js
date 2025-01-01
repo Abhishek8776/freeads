c404f533387266d9ea38f9064879333335748ded
Abhishek8776 <abhishekpclt1999@gmail.com>
2025-01-02 01:11:02 +0530

    port remove

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const instance = axios.create({
-  baseURL: 'http://13.201.137.144:8000/',
+  baseURL: 'http://13.201.137.144/',
 });
 
 instance.interceptors.request.use((config) => {
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "http://13.201.137.144:8000";
+export const BACKEND_URL = "http://13.201.137.144";
